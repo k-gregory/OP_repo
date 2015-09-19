423815a79442e9704562096bf39540e2df33ef0e
Gregory Komar <xalvevs@gmail.com>
2015-09-19 14:52:01 +0300

    C

--- courses/lessons/lesson1/w.c
+++ courses/lessons/lesson1/w.c
@@ -1,10 +1,8 @@
-/*#include <stdio.h>
+#include <stdio.h>
 
 int main(void){
   printf("char is %i\n",sizeof(char));
   printf("int is %i\n",sizeof(int));
   printf("pointer is %i\n",sizeof(void*));
   return 0;
-}*/
-
-main;
+}
