f2e63e458de0263ec43c651aeaf7b6cc1f973ae3
Gregory K <xalvevs@gmail.com>
2015-09-20 10:45:06 +0300

    Test branches k

--- courses/lessons/lesson1/w.c
+++ courses/lessons/lesson1/w.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main(void){
-  printf("char is %i\n",sizeof(char));
-  printf("int is %i\n",sizeof(int));
-  printf("pointer is %i\n",sizeof(void*));
+int main(void) {
+  printf("char is %i\n", sizeof(char));
+  printf("int is %i\n", sizeof(int));
+  printf("pointer is %i\n", sizeof(void *));
+  printf("double is %i\n", sizeof(double));
   return 0;
 }
