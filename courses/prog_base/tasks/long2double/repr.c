a59944a3c349c39458c769a8e754da43d351cb59
Gregory K <xalvevs@gmail.com>
2015-10-12 00:09:08 +0300

    Unfair long2double(no casts/ptrs)

--- courses/prog_base/tasks/long2double/repr.c
+++ courses/prog_base/tasks/long2double/repr.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include <assert.h>
 
-double long2double(long long x){
-  assert(sizeof(double)==sizeof(long long));
-  return *((double*)&x);
+union {
+  double d;
+  long long ll;
+} caster;
+
+double no_ptr_casts_but_unfair_long2double(long long x) {
+  caster.ll = x;
+  return caster.d;
 }
 
-int main(){
-
-  long long a = 20;
-
-  printf("%.30f, %.30f\n",
-      long2double(4620000000000000000),
-      long2double(-4620000000000000000));
-
-  return 0;
-}
+static const int ll_size_equals_double_size_check =
+    1 / ((sizeof(double) - sizeof(long long)) == 0);
