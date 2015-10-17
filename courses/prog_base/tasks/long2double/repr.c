d1912ad633c662f00a77c22bd85a3d64d34368ef
Gregory K <xalvevs@gmail.com>
2015-10-17 23:57:32 +0300

    long2double

--- courses/prog_base/tasks/long2double/repr.c
+++ courses/prog_base/tasks/long2double/repr.c
@@ -8,26 +8,27 @@ static const int ll_size_equals_double_size_check =
 
 double hack_long2double(long long x) { return *((double *)&x); }
 
-#define nth_byte(num, n) (((((num)) & (1LL << (64 - n))) > 0))
+static inline int nth_byte(long long num, int n){
+  return (num&(1LL<<(64-n)))==1LL<<(64-n);
+}
 
 double long2double(long long x) {
+  double sign = nth_byte(x,1) ? -1 : 1;
   double mantissa = 1;
-  unsigned int exp = 0;
-
-  int sign = nth_byte(x, 1) ? -1 : 1;
+  double exp = 0;
 
   int p = 1;
   for (int i = 12; i > 1; i--) {
     exp += nth_byte(x, i) * p;
     p *= 2;
   }
+
   double pw = 0.5;
   for (int i = 13; i <= 64; i++) {
     mantissa += nth_byte(x, i) * pw;
     pw /= 2;
   }
-  /*EXP==2047*/
-  if (exp == 0b11111111111) {
+  if (exp == 2047) {
     if (mantissa != 0)
       return NAN;
     if (sign > 0)
--- courses/prog_base/tasks/long2double/test.c
+++ courses/prog_base/tasks/long2double/test.c
@@ -1,22 +1,55 @@
 #include <check.h>
 #include <stdio.h>
 #include <limits.h>
+#include <math.h>
+
 #include "repr.h"
 
-#define STEP 5000000
+static void checkl2d(long long x){
+  if(isnan(long2double(x))&&isnan(hack_long2double(x)))
+    return;
+  double my_res = long2double(x);
+  double right_res = hack_long2double(x);
+  ck_assert(my_res==right_res);
+}
 
-START_TEST(long2double_test) {
-  for (long long x = -LLONG_MIN; x < LLONG_MAX; x += STEP) {
-    ck_assert(long2double(x) == hack_long2double(x));
-  }
+START_TEST(long2double_test_fft) {
+  checkl2d(15LL);
 }
 END_TEST
+START_TEST(long2double_test_llmin) {
+  checkl2d(LLONG_MIN);
+}
+END_TEST
+START_TEST(long2double_test_llmax) {
+  checkl2d(LLONG_MAX);
+}
+END_TEST
+START_TEST(long2double_test_zero) {
+  checkl2d(0LL);
+}
+END_TEST
+START_TEST(long2double_test_val) {
+  checkl2d(4620000000000000000LL);
+}
+END_TEST
+START_TEST(long2double_test_neg_val) {
+  checkl2d(-4620000000000000000LL);
+}
+END_TEST
+
+#undef checkl2d
 
 int main(void) {
   Suite *suite = suite_create("Long2Double");
   TCase *tcase = tcase_create("Long2Double");
   tcase_set_timeout(tcase, 60 * 10);
-  tcase_add_test(tcase, long2double_test);
+  tcase_add_test(tcase, long2double_test_fft);
+  tcase_add_test(tcase, long2double_test_llmin);
+  tcase_add_test(tcase, long2double_test_llmax);
+  tcase_add_test(tcase, long2double_test_zero);
+  tcase_add_test(tcase, long2double_test_val);
+  tcase_add_test(tcase, long2double_test_neg_val);
   suite_add_tcase(suite, tcase);
   SRunner *sr = srunner_create(suite);
   srunner_run_all(sr, CK_VERBOSE);
