ffb680a7700dc395e3b3d9c2d3f0d596b59dfebc
Gregory Komar <xalvevs@gmail.com>
2015-10-17 12:33:10 +0300

    Long2Double

--- courses/prog_base/tasks/long2double/repr.c
+++ courses/prog_base/tasks/long2double/repr.c
@@ -8,7 +8,15 @@ double hack_long2double(long long x){
   return *((double*)&x);
 }
 
+double long2double(long long x){
+  char buff[1000];
+  sprintf(buff,"%f",x);
+  double res;
+  sscanf(buff,"%lf",&res);
+  return res;
+}
+
 #define nth_byte(num,n) ((num))&(1<<(n))
 
-double long2double(long long x){
-}
+/*double long2double(long long x){
+}*/
