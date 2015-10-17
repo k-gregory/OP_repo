e4c1c43c7d7eeb242eaaeea8414efa6fa3911936
Gregory K <xalvevs@gmail.com>
2015-10-17 22:56:59 +0300

    Long2Double

--- courses/prog_base/tasks/long2double/repr.c
+++ courses/prog_base/tasks/long2double/repr.c
@@ -1,25 +1,45 @@
 #include "repr.h"
 
-#include <stdio.h>
 #include <assert.h>
+#include <math.h>
 
 static const int ll_size_equals_double_size_check =
     1 / ((sizeof(double) - sizeof(long long)) == 0);
 
-double hack_long2double(long long x){
-  assert(sizeof(double)==sizeof(long long));
-  return *((double*)&x);
+double hack_long2double(long long x) { return *((double *)&x); }
+
+#define nth_byte(num, n) (((((num)) & (1LL << (64 - n))) > 0))
+
+double long2double(long long x) {
+  double mantissa = 1;
+  unsigned int exp = 0;
+
+  int sign = nth_byte(x, 1) ? -1 : 1;
+
+  int p = 1;
+  for (int i = 12; i > 1; i--) {
+    exp += nth_byte(x, i) * p;
+    p *= 2;
+  }
+  double pw = 0.5;
+  for (int i = 13; i <= 64; i++) {
+    mantissa += nth_byte(x, i) * pw;
+    pw /= 2;
+  }
+  /*EXP==2047*/
+  if (exp == 0b11111111111) {
+    if (mantissa != 0)
+      return NAN;
+    if (sign > 0)
+      return INFINITY;
+    return -INFINITY;
+  }
+  if (exp == 0) {
+    if (mantissa != 0)
+      return sign * pow(2, -1022) * (mantissa - 1);
+    if (sign > 0)
+      return 0;
+    return -0.0;
+  }
+  return sign * pow(2, exp - 1023) * mantissa;
 }
-
-double long2double(long long x){
-  char buff[1000];
-  sprintf(buff,"%f",x);
-  double res;
-  sscanf(buff,"%lf",&res);
-  return res;
-}
-
-#define nth_byte(num,n) ((num))&(1<<(n))
-
-/*double long2double(long long x){
-}*/
--- courses/prog_base/tasks/long2double/test.c
+++ courses/prog_base/tasks/long2double/test.c
@@ -3,26 +3,23 @@
 #include <limits.h>
 #include "repr.h"
 
-START_TEST (long2double_test){
-  char buf1[1000];
-  char buf2[1000];
+#define STEP 5000000
 
-  for(long long i = LONG_MIN;i<LONG_MAX-50001;i+=50000){
-    double a = hack_long2double(i);
-    double b = long2double(i);
-    sprintf(buf1,"%f",a);
-    sprintf(buf2,"%f",b);
-    ck_assert_str_eq(buf1,buf2);
+START_TEST(long2double_test) {
+  for (long long x = -LLONG_MIN; x < LLONG_MAX; x += STEP) {
+    ck_assert(long2double(x) == hack_long2double(x));
   }
-} END_TEST
+}
+END_TEST
 
-int main(void){
-  Suite* suite = suite_create("Long2Double");
-  TCase* tcase  = tcase_create("Long2Double");
-  tcase_add_test(tcase,long2double_test);
-  suite_add_tcase(suite,tcase);
-  SRunner* sr = srunner_create(suite);
-  srunner_run_all(sr,CK_VERBOSE);
+int main(void) {
+  Suite *suite = suite_create("Long2Double");
+  TCase *tcase = tcase_create("Long2Double");
+  tcase_set_timeout(tcase, 60 * 10);
+  tcase_add_test(tcase, long2double_test);
+  suite_add_tcase(suite, tcase);
+  SRunner *sr = srunner_create(suite);
+  srunner_run_all(sr, CK_VERBOSE);
   int fails = srunner_ntests_failed(sr);
   srunner_free(sr);
   return fails;
