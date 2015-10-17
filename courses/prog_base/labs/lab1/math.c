69cd8e3ba8d1bcb465b1a0cd10cd928718a6b5b4
Gregory Komar <xalvevs@gmail.com>
2015-10-17 10:44:28 +0300

    Lab

--- courses/prog_base/labs/lab1/math.c
+++ courses/prog_base/labs/lab1/math.c
@@ -1,12 +1,16 @@
 #include <stdlib.h>
+#include <errno.h>
 #include <tgmath.h>
 
 #include "config.h"
 
 double calc(double x, double y, double z) {
+  errno=0;
   double a0 = pow(x, y + 1.0) / pow(x - y, 1.0 / z);
   double a1 = y / ((N0 + 1.0) * fabs(x + y));
   double a2 = pow(x + N1, 1.0 / fabs(sin(y)));
+  
+  if(errno) return NAN; 
 
   return a0 + a1 + a2;
 }
