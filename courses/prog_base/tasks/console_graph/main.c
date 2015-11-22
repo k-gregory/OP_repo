5335e66ea4e67fa5fb71b579b25bbb8787496dc4
Gregory Komar <xalvevs@gmail.com>
2015-11-22 15:07:12 +0200

    Console graph

--- courses/prog_base/tasks/console_graph/main.c
+++ courses/prog_base/tasks/console_graph/main.c
@@ -13,24 +13,28 @@
 
 double x_coef;
 int mx, my;
+int max_y, max_x;
 double step;
 
 typedef double (*p_func)(double);
 
-double f_val(p_func f, double x) { return f((x+mx) / x_coef) * x_coef + my; }
+double f_val(p_func f, double x) { return f((x + mx) / x_coef) * x_coef + my; }
 
-void put_point(double y, double x) { mvaddch(ceil(y), ceil(x), PLOT_CHAR); }
+static inline int get_x(double x) { return nearbyint(x); }
+static inline int get_y(double y) { return max_y - nearbyint(y); }
+
+void put_point(double y, double x) { mvaddch(get_y(y), get_x(x), PLOT_CHAR); }
 
 void draw(p_func f, double l_x, double r_x, int color_pair) {
   attron(COLOR_PAIR(color_pair));
   for (double x = l_x; x < r_x; x += step) {
     double y = f_val(f, x);
+    if (isnan(y))
+      continue;
     put_point(y, x);
   }
   refresh();
 }
-
-double test(double x) { return 1; }
 
 int main(void) {
   int x, y;
@@ -43,17 +47,17 @@ int main(void) {
   init_pair(PLOT_COLOR_1, COLOR_BLACK, COLOR_YELLOW);
   init_pair(PLOT_COLOR_2, COLOR_BLACK, COLOR_BLUE);
 
-  getmaxyx(stdscr, y, x);
-  mx = -x / 2;
-  my = y / 2;
+  getmaxyx(stdscr, max_y, max_x);
+  mx = -max_x / 2;
+  my = max_y / 2;
   x_coef = 3;
-  step = 0.2;
+  step = 0.02;
 
-  draw(sin, 0, x, PLOT_COLOR_1);
-  mx-=3;
-  draw(cos, 0, x, PLOT_COLOR_2);
+  draw(sin, 0, max_x, PLOT_COLOR_1);
+  draw(cos, 0, max_x, PLOT_COLOR_2);
+  draw(sqrt, 0, max_x, PLOT_COLOR_1);
 
-  getchar();
+  getch();
   endwin();
 
   return 0;
