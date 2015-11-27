e2f7fbcf06ed13401384e16f50c83865b7e9ef57
Gregory Komar <xalvevs@gmail.com>
2015-11-27 09:48:33 +0200

    Sort

--- opi/lessons/Nov27/main.c
+++ opi/lessons/Nov27/main.c
@@ -3,10 +3,12 @@
 
 #define LEN(arr) (sizeof(arr) / sizeof((arr)[0]))
 
-typedef int f_cmp(void *a, void *b);
+typedef int f_cmp(const void *a, const void *b);
 typedef void f_swap(void *a, void *b);
 
-int cmp_ints(void *a, void *b) { return (int *)a - (int *)b; }
+int cmp_ints(const void *a, const void *b) {
+  return *(const int *)a - *(const int *)b;
+}
 
 void swap_ints(void *a, void *b) {
   int t = *(int *)a;
@@ -16,17 +18,17 @@ void swap_ints(void *a, void *b) {
 
 void my_sort(void *arr, size_t el_count, size_t el_size, f_cmp cmp_cb,
              f_swap swap_cb) {
-  for (int i = 0; i < el_count - 1; i++)
-    for (int j = 0; j < el_count - i - 1; j++) {
+  for (size_t i = 0; i < el_count - 1; i++)
+    for (size_t j = 0; j < el_count - i - 1; j++) {
       void *curr = (char *)arr + el_size * j;
       void *next = (char *)arr + el_size * (j + 1);
-      if (cmp_cb(curr, next))
+      if (cmp_cb(curr, next)>0)
         swap_cb(curr, next);
     }
 }
 
 void view_arr(int *arr, size_t el_count) {
-  for (int i = 0; i < el_count; i++)
+  for (size_t i = 0; i < el_count; i++)
     printf("%d ", arr[i]);
   puts("");
 }
