fcff3a63642cfc14a530342e12c0d724542125f7
Gregory Komar <xalvevs@gmail.com>
2015-12-25 09:27:28 +0200

    Lab 5

--- opi/lab5_arm/impl.s
+++ opi/lab5_arm/impl.s
@@ -4,23 +4,14 @@ impl:
 mov ip, 0
 str ip, [r3]
 
-smull r0,ip,r0,r1
-cmp ip, r0, ASR #31
-bne mul_overflow
-b substraction
+mul r0, r0, r1
 
-mul_overflow:
-mov ip, 1
-str ip, [r3]
-b substraction
-
-substraction:
-sub r0,r0,r2
+subs r0,r0,r2
 bvs  sub_overflow
 str ip, [r3]
 bx lr
 
 sub_overflow:
-mov ip,#42
+mov ip,#1
 str ip, [r3]
 bx lr
--- opi/lab5_arm/main.c
+++ opi/lab5_arm/main.c
@@ -4,8 +4,20 @@
 #include <stdio.h>
 #include <limits.h>
 
+#define BUFF_LEN 50
+#define LEN(arr) (sizeof(arr)/sizeof((arr)[0]))
+
 int main(void){
   int overflowed;
-  printf("%d overflow:%d\n", impl(-INT_MAX,1,INT_MAX, &overflowed), overflowed);
+  char buff[BUFF_LEN];
+  int a,b,c;
+  printf("INT_MIN: %d\n", INT_MIN);
+
+  do {
+    puts("Enter \"a b c\"");
+    fgets(buff, LEN(buff), stdin);
+  } while(sscanf(buff,"%d %d %d",&a, &b, &c)!=3);
+
+  printf("a*b-c=%d overflow:%d\n", impl(a,b,c, &overflowed), overflowed);
   return EXIT_SUCCESS;
 }
