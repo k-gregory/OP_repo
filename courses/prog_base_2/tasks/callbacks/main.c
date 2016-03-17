8db71ba6a195b9d9cc569ff9708a9ec3b759dd02
Gregory Komar <xalvevs@gmail.com>
2016-03-17 19:18:16 +0200

    Improvements

--- courses/prog_base_2/tasks/callbacks/main.c
+++ courses/prog_base_2/tasks/callbacks/main.c
@@ -3,19 +3,48 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#define LEN(arr) (sizeof(arr)/sizeof((arr)[0]))
+
 typedef void (*text_f)(const char* text);
 
-void sent_fl(const char* text){
+void sent_print_cb(const char* text){
   printf("Sentence: \"%s\"\n", text);
 }
 
-void word_fl(const char* text){
+void text_word_length_cb(const char* _text){
+  const char* text = _text;
+  size_t str_words_len = 0;
+  while(*text != '\0'){
+    if(isalpha(*text))
+      ++str_words_len;
+    ++text;
+  }
+  printf("\"%s\" length of words: %zu\n",_text,str_words_len);
+}
+
+void bad_word_checker_cb(const char* text){
+  const char* const bad_words[] =
+    {"assembly", "Windows", "matan", "kursach", "deadline"};
+  for(size_t i = 0 ; i < LEN(bad_words); ++i)
+    if(strcasecmp(text,bad_words[i]) == 0){
+      printf("A bad word found: %s\n", text);
+      break;
+    }
+}
+
+void word_print_cb(const char* text){
   printf("Word: \"%s\"\n", text);
+}
+
+static void noop_text_cb(const char* text){
 }
 
 void process(const char* _str, text_f sentenceCB, text_f wordCB){
   char* str = malloc(strlen(_str)+1);
   strcpy(str, _str);
+
+  if(!sentenceCB) sentenceCB = noop_text_cb;
+  if(!wordCB) wordCB = noop_text_cb;
 
   char* tokenized = strtok(str,".?!");
   while(tokenized){
@@ -50,6 +79,15 @@ int main(void){
   process("Oh, lol! Tthis is insanity. " \
       "  What are me doein here?   " \
       "I love rabbits????  Penguins  "
-      ,sent_fl, word_fl);
+      ,sent_print_cb, word_print_cb);
+  
+  puts("\n#####Another launch ######\n");
+  
+  process("I have just done some matan."\
+	  "It is time to write some ASSembly"	\
+          " for !!WinDoWS?!!. It is going to be"\
+	  " my kursach?Writting assembly is the"\
+	  " best way to fail deadline"
+	  ,text_word_length_cb, bad_word_checker_cb);
   return EXIT_SUCCESS;
 }
