f6b455678809bd873bf43387d2da86823c9d771d
Gregory Komar <xalvevs@gmail.com>
2016-04-09 10:25:03 +0300

    loop

--- courses/prog_base_2/labs/lab1/main.c
+++ courses/prog_base_2/labs/lab1/main.c
@@ -1,4 +1,5 @@
 #include "univ_person.h"
+#include "univsec_wicket.h"
 #include "univ_term.h"
 
 #include <stdlib.h>
@@ -6,26 +7,31 @@
 
 #define LEN(arr) (sizeof(arr)/sizeof((arr)[0]))
 
-int main(void){
-  char name_buff[20];
-  univ_person* passers[10];
-  univ_term* term  = univ_term_new(20,4,UNIV_PERSON_TEACHER);
-  for(size_t i = 0; i < LEN(passers); i++){
-    sprintf(name_buff,"P%zu",i);
-    passers[i] = univ_person_new(name_buff, UNIV_PERSON_TEACHER);
-  }
-  univ_term_send_passers(term,passers, 10);
-  univ_term_display(term);
-  univ_term_send_passers(term,passers, 2);
-  univ_term_display(term);
-  univ_term_send_passers(term,passers+4, 3);
-  univ_term_send_passers(term,passers,1);
-  univ_term_display(term);
-  univ_term_send_passers(term,passers, 10);
-  univ_term_display(term);
 
-  for(size_t i = 0; i < LEN(passers); i++){
-    univ_person_free(passers[i]);
+int main(void){
+  char buff[256];
+
+  univ_term* t1 = univ_term_new(5,1,UNIV_PERSON_STUDENT);
+  univ_term* t2 = univ_term_new(5,2,UNIV_PERSON_STUDENT | UNIV_PERSON_TEACHER);
+  univ_term* t3 = univ_term_new(5,3,UNIV_PERSON_SERVICE_STUFF);
+
+  univsec_wicket* w1 = univsec_wicket_new();
+  univsec_wicket* w2 = univsec_wicket_new();
+
+  while(1){
+    char name_buff[256];
+    unsigned int id,cat;
+    fgets(buff, LEN(buff), stdin);
+    if(strcmp(buff,"exit")) break;
+    if(sscanf(buff,"pass %u \n")==1){
+
+    }
   }
-  univ_term_free(term);
+
+  univsec_wicket_free(w1);
+  univsec_wicket_free(w2);
+
+  univ_term_free(t1);
+  univ_term_free(t2);
+  univ_term_free(t3);
 }
