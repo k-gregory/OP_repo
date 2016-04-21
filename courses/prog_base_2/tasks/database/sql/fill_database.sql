a5487b00a25dd2eb0d0d9370f25dddf4f1aa1bda
Gregory K <xalvevs@gmail.com>
2016-04-22 01:31:56 +0300

    Fill db

---
+++ courses/prog_base_2/tasks/database/sql/fill_database.sql
@@ -0,0 +1,8 @@
+insert into Driver(name,surname,car_manufacturer,car_model,car_average_speed,km_fare,satisfied_clients,unsatisfied_clients)
+values ("Fabiola","Nicolosi","BMW","F30","70.5","1.45","56","12");
+insert into Driver(name,surname,car_manufacturer,car_model,car_average_speed,km_fare,satisfied_clients,unsatisfied_clients)
+values ("Fabiola","Nicolosi","BMW","F30","70.5","1.45","56","12");
+insert into Driver(name,surname,car_manufacturer,car_model,car_average_speed,km_fare,satisfied_clients,unsatisfied_clients)
+values ("Fabiola","Nicolosi","BMW","F30","70.5","1.45","56","12");
+insert into Driver(name,surname,car_manufacturer,car_model,car_average_speed,km_fare,satisfied_clients,unsatisfied_clients)
+values ("Fabiola","Nicolosi","BMW","F30","70.5","1.45","56","12");
