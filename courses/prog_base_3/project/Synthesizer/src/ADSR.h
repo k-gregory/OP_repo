a18ab1d89e6a72f31f7a08961c54a3c19e44931f
Gregory K <xalvevs@gmail.com>
2016-05-05 20:29:44 +0300

    Remove old C files

--- courses/prog_base_3/project/Synthesizer/src/ADSR.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <SDL.h>
-
-typedef void (*synth_adsr_callback)(void* data, Uint8* stream, unsigned int length);
--- courses/prog_base_3/project/Synthesizer/src/audio_utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-/**
-\param[in] HzFreq Wanted frequency in hertz
-\param[in] samples_per_second DSP frequency
-\returns Value which can be multiplied by sample index to get wave time argument
- */
-float synth_get_freq(unsigned int HzFreq,
-		     unsigned int samples_per_second);
