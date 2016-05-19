3c60e130dfe41b45b84e9660d4996475b55adc11
Gregory Komar <xalvevs@gmail.com>
2016-05-19 09:36:14 +0300

    Bad tests

--- opi/HashTable/src/test/java/io/github/atommed/ads/MapTests.java
+++ opi/HashTable/src/test/java/io/github/atommed/ads/MapTests.java
@@ -1,40 +1,94 @@
 package io.github.atommed.ads;
 
-import com.google.common.collect.testing.MapTestSuiteBuilder;
-import com.google.common.collect.testing.TestStringMapGenerator;
-import com.google.common.collect.testing.features.CollectionSize;
-import com.google.common.collect.testing.features.MapFeature;
-import junit.framework.TestSuite;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-import java.util.Map;
 
 /**
  * Created by gregory on 19.05.16.
  */
-@RunWith(Suite.class)
-@Suite.SuiteClasses(MapTests.GuavaTests.class)
+
 public class MapTests {
-    public static class GuavaTests{
-        public static TestSuite suite(){
-            return MapTestSuiteBuilder.using(new TestStringMapGenerator(){
-                @Override
-                protected Map<String, String> create(Map.Entry<String, String>[] entries) {
-                    Map<String, String> map = new HashMap<>(entries.length);
-                    for(Map.Entry<String , String > e: entries)
-                        map.put(e.getKey(),e.getValue());
-                    return map;
-                }
-            })
-                    .named("HashMap tests")
-                    .withFeatures(
-                            MapFeature.ALLOWS_ANY_NULL_QUERIES,
-                            MapFeature.SUPPORTS_PUT,
-                            MapFeature.SUPPORTS_REMOVE,
-                            CollectionSize.ANY
-                    )
-                    .createTestSuite();
-        }
+  private HashMap<String, String> m;
+  private HashMap<String, String> getEmptyMap(){
+    return new HashMap<String, String>();
+  }
+  private HashMap<String, String> getOneElementMap(){
+    HashMap<String, String> ret =  new HashMap<>();
+    ret.put("Hello,", "HashMap");
+    return ret;
+  }
+  private HashMap<String, String> get10ElementMap(){
+    HashMap<String, String> ret = new HashMap<>();
+    for(int i = 0; i < 10; i++)
+      ret.put(Integer.toString(i),Integer.toString(i*i));
+    return ret;
+  }
+
+  @Test
+  public void emptySizeRetZero(){    
+    m = getEmptyMap();
+    assertEquals(m.size(), 0);
+  }
+  @Test
+  public void OneElementSizeRetOne(){    
+    m = getOneElementMap();
+    assertEquals(m.size(), 1);
+  }
+
+  @Test
+  public void TenElementSizeRetTen(){
+    m = get10ElementMap();
+    assertEquals(m.size(), 10);
+  }
+
+
+  @Test
+  public void addChangesSize(){
+    m = getEmptyMap();
+    m.put("Test", "1");
+    assertEquals(m.size(), 1);
+  }
+
+  @Test public void addValuesSaved(){
+    m = getEmptyMap();
+    for(int i = 0; i < 50; i++)
+      m.put(Integer.toString(i), Integer.toString(i*i)+"VAL");
+    for(int i = 49; i >= 50; i++)
+      assertEquals(m.get(Integer.toString(i)), Integer.toString(i*i)+"VAL");
+  }
+
+  class CollisionFull{
+    public int val;
+
+    public CollisionFull(int val){
+      this.val = val;
     }
+
+    @Override
+    public int hashCode(){
+      return 42;
+    }
+    @Override
+    public boolean equals(Object o){
+      if(o == null) return false;
+      return this.val == ((CollisionFull) o).val;
+    }
+  }
+
+  @Test public void collisionsHandled(){
+   HashMap<CollisionFull, String> dict = new HashMap<>();
+   for(int i = 0; i < 50; i++)
+     dict.put(new CollisionFull(i), Integer.toString(i*i)+"VAL");
+   for(int i = 49; i >= 50; i++)
+     assertEquals(dict.get(new CollisionFull(i)), Integer.toString(i*i)+"VAL");
+  }
+
+  @Test 
+  public void removeWorks(){
+    m = getEmptyMap();
+    m.put("A", "B");
+    assertEquals(m.remove("A"), "B");
+    assertEquals(m.size(), 0);
+  }
 }
